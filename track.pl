741a95140d027f257367d8e38f3d29140f31831b
jeff harlan <harlan@yahoo-inc.com>
2014-11-03 10:01:36 -0800

    comments and cleanup

--- track.pl
+++ track.pl
@@ -6,13 +6,15 @@ use WWW::Mechanize;
 use JSON;
 use Math::Trig qw(great_circle_distance great_circle_bearing deg2rad rad2deg cotan atan tan);
 
-my $ip = "10.20.30.249";
-my $port = "8080";
+my $ip = "10.20.30.249";	# IP address of your piaware device
+my $port = "8080";		# default port for piaware
 my @arr;
 
-my $lat=37.2267;
-my $lon=-121.9736;
-my $alt=450;
+my $lat=37.2267;		# your latitude
+my $lon=-121.9736;		# your longitude
+my $alt=450;			# your altitude
+
+my $last_contact=20;		# max seconds since last contact to continue tracking
 
 my $uri = "http://$ip:$port/dump1090/data.json";
 my $now = time();
@@ -35,7 +37,7 @@ eval {
 	my $text = $json->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($www->content());
 	foreach my $entry (@{$text}) {
 		@arr = values %{$entry};
-		if ( $arr[1] ) {
+		if ( $arr[1] && ( $arr[7] <= $last_contact )) {
 			print "$now:@arr:";
 			@them = nesw( $arr[10], $arr[3] );
 			$km = great_circle_distance( @us, @them, 6378 );
